ff5f57b9a7dba5a4296bf4ebcacdfd5f735ffed1
Scott Crowder <scrowder@gmail.com>
2024-08-27 12:19:15 -0500

    Initial Commit

---
+++ Drag.And.Drop/Drag_And_DropApp.swift
@@ -0,0 +1,20 @@
+//
+// File: Drag_And_DropApp.swift
+// Project: Drag.And.Drop
+// 
+// Created by SCOTT CROWDER on 8/27/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct Drag_And_DropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
